ed736748bd7b4d30d8cd9e2ffcd9ef0a361f0430
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-11-24 01:54:52 +0630

    feat: add events console project

---
+++ src/Events.ConsoleApp/Events.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+using Newtonsoft.Json;
+
+int totalDays = DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365;
+var list = new List<Dictionary<int, string>>();
+for (int i = 1; i <= totalDays; i++)
+{
+    list.Add(new Dictionary<int, string>
+    {
+        { i, Ulid.NewUlid().ToString() } 
+    });
+}
+string json = JsonConvert.SerializeObject(list, Newtonsoft.Json.Formatting.Indented);
+Console.WriteLine(json);
+Console.ReadLine();
